7434f20082154bc649fe975d2f88e61c4f3f84b0
Laiba nurr <laibasirahh7@gmail.com>
2026-09-14 17:21:58 +0500

    Add Project model with Firestore serialization

---
+++ lib/models/project.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Project {
+  final String projectName;
+  final String projectStatus;
+  final int rateInCents;
+  final DateTime startDate;
+  final DateTime endDate;
+  final String clientId;
+  final String id;
+
+  Project({
+    required this.clientId,
+    required this.endDate,
+    required this.projectName,
+    required this.projectStatus,
+    required this.rateInCents,
+    required this.startDate,
+    required this.id,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'clientId': clientId,
+      'rateInCents': rateInCents,
+      'endDate': endDate,
+      'startDate': startDate,
+      'projectStatus': projectStatus,
+      'projectName': projectName,
+    };
+  }
+
+  factory Project.fromMap(Map<String, dynamic> map) {
+    return Project(
+      clientId: map['clientId'] as String,
+      endDate: (map['endDate'] as Timestamp).toDate(),
+      projectName: map['projectName'] as String,
+      projectStatus: map['projectStatus'] as String,
+      rateInCents: map['rateInCents'] as int,
+      startDate: (map['startDate'] as Timestamp).toDate(),
+      id: map['id'] as String,
+    );
+  }
+}
